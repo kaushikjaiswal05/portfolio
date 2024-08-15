416fe48cdf7bb39029ef6dcde03b07fc82ede090
Kaushik Jaiswal <kaushikjaiswal024@gmail.com>
2024-08-15 12:54:29 +0530

    Config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/portfolio/",
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' && process.env.GH_PAGES === 'true' ? "/portfolio/" : "/",
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+    },
+  };
 });
